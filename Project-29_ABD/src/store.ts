357a3e87f35599514eca9989884f799f998e2fe0
danielshmidov <daniel.shmidov@gmail.com>
2024-06-05 11:09:35 -0700

    refractored develop folder to hold breabones vite project

---
+++ Project-29_ABD/src/store.ts
@@ -0,0 +1,11 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+const store = configureStore({
+  reducer: {
+    // Add your reducers here
+  },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+export default store;
