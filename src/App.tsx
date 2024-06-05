7ce6dba3e217ed4ecee4c86d700fdf9f9379cf65
AndrewJoji <andrewjoji71@gmail.com>
2024-06-05 16:31:04 -0700

    added basic component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,29 @@
 //import React from "react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
 
 function App() {
   return (
     <div className="App">
+      <Card>
+        <CardHeader>
+          <CardTitle>Solo Explorer</CardTitle>
+          <CardDescription>Discover the world, discover yourself</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>Card Content</p>
+        </CardContent>
+        <CardFooter>
+          <p>Card Footer</p>
+        </CardFooter>
+      </Card>
+
     </div>
   );
 }
