d8ba2200bf1c5459c389d6e9682f0daefa79dcbc
AndrewJoji <andrewjoji71@gmail.com>
2024-07-09 15:21:57 -0700

    added   <DndProvider backend={HTML5Backend}> context wrapper arround <App />

--- src/main.tsx
+++ src/main.tsx
@@ -6,8 +6,12 @@ import "./index.css";
 import App from "./App.tsx";
 import Header from "./components/layout/header.tsx";
 import Sidebar from "./components/layout/sidebar.tsx";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from 'react-dnd-html5-backend';
+
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <DndProvider backend={HTML5Backend}>
   <React.StrictMode>
     <>
       <Header />
@@ -20,4 +24,5 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     </>
 
   </React.StrictMode>
+  </DndProvider>
 );
