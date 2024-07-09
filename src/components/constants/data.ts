2777b9abb39006e42c616d012c6a44a5d4dafdbf
bhagcheema <bhagkcheema@gmail.com>
2024-07-08 23:05:52 -0400

    updated files to render

---
+++ src/components/constants/data.ts
@@ -0,0 +1,17 @@
+import { NavItem } from "../../types";
+
+
+export const navItems: NavItem[] = [
+  {
+    title: "Dashboard",
+    href: "/dashboard",
+    icon: "dashboard",
+    label: "Dashboard",
+  },
+  {
+    title: "Login",
+    href: "/",
+    icon: "login",
+    label: "login",
+  },
+];
---
+++ src/main.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./redux/store.ts"
+import "./index.css";
+import App from "./App.tsx";
+import Header from "./components/layout/header.tsx";
+import Sidebar from "./components/layout/sidebar.tsx";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <>
+      <Header />
+      <div className="flex h-screen overflow-hidden">
+        <Sidebar />
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </div>
+    </>
+
+  </React.StrictMode>
+);
