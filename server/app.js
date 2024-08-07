107510fabaabd10ea6c74a0a109e5ab598f6ba8e
bhagcheema <bhagkcheema@gmail.com>
2024-08-07 10:18:35 -0400

    working version

--- server/app.js
+++ server/app.js
@@ -24,7 +24,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 // app.use(cors());
 app.use(cors({
-    origin: 'http://localhost:5173', // frontend's origin
+    origin: 'http://localhost:5174', // frontend's origin
     credentials: true // Enable sending cookies with CORS requests
 }));
 app.use(express.static(path.join(__dirname, 'public')));
--- server/models/user.js
+++ server/models/user.js
@@ -1,4 +1,18 @@
+// const mongoose = require('mongoose');
+// const Schema = mongoose.Schema;
+//
+// const userSchema = new Schema({
+//     email: { type: String, required: true, unique: true },
+//     password: { type: String, required: true },
+// });
+//
+// const User = mongoose.model('User', userSchema);
+//
+// module.exports = User;
+
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
@@ -6,6 +20,22 @@ const userSchema = new Schema({
     password: { type: String, required: true },
 });
 
+// Method to generate JWT
+userSchema.methods.generateAuthToken = function () {
+    const token = jwt.sign({ id: this._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    return token;
+};
+
+// Method to hash password before saving user
+userSchema.pre('save', async function (next) {
+    if (this.isModified('password')) {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+    }
+    next();
+});
+
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
+
