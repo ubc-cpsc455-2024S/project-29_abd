4946f30a92fc905889850a9d2a51cafd7a095d6c
bhagcheema <bhagkcheema@gmail.com>
2024-08-17 10:44:47 -0400

    changing vite config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,24 @@
+// import path from "path";
+// import react from "@vitejs/plugin-react";
+// import { defineConfig } from "vite";
+// import envCompatible from 'vite-plugin-env-compatible';
+//
+//
+// export default defineConfig({
+//   plugins: [react(), envCompatible()],
+//   define: {
+//     'process.env': {}
+//   },
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// });
 import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import envCompatible from 'vite-plugin-env-compatible';
-
 
 export default defineConfig({
   plugins: [react(), envCompatible()],
@@ -14,4 +30,10 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    sourcemap: true, // Generates sourcemaps for easier debugging
+    cssCodeSplit: true, // Ensures CSS is split per component where possible
+  },
+  base: '/', // Adjust base path if necessary
 });
+
