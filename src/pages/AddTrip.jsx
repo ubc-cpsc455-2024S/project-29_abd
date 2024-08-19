06ca2a805cb36b010c9233405e2de3cafaf44172
bhagcheema <bhagkcheema@gmail.com>
2024-08-18 21:44:55 -0400

    fixing bug in add-trip

--- src/pages/AddTrip.jsx
+++ src/pages/AddTrip.jsx
@@ -17,7 +17,6 @@
 // } from "../components/ui/card";
 // import MapWithMarkers from "../services/api/map-template";
 // import DayTimeline from "../components/dayTimeline";
-// //import { fetchDayCards } from "../redux/dayTimelineSlice";
 // import { fetchDayCards, clearDayCards } from "../redux/dayTimelineSlice";
 // import { Button } from "../components/ui/button";
 //
@@ -28,6 +27,7 @@
 //   const dayCards = useSelector((state) => state.dayTimeline.dayCards);
 //   const [allLocations, setAllLocations] = useState([]);
 //   const printRef = useRef(); // Reference for the day cards section
+//   const dayTimelineRef = useRef(); // Ref to DayTimeline
 //
 //   useEffect(() => {
 //     if (tripId && token) {
@@ -49,137 +49,130 @@
 //     }
 //   }, [dayCards]);
 //
-//   // const handlePrint = () => {
-//   //   setTimeout(() => {
-//   //     const printContent = printRef.current.innerHTML;
-//   //     const originalContent = document.body.innerHTML;
-//   //
-//   //     document.body.innerHTML = printContent;
-//   //     window.print();
-//   //     document.body.innerHTML = originalContent;
-//   //     window.location.reload(); // Reload the page to restore the original content
-//   //   }, 500);
-//   // };
-//
 //   const handlePrint = () => {
-//     const printContent = document.querySelector('.day-timeline'); // Select the specific element you want to print
-//     if (!printContent) return; // If the element doesn't exist, exit the function
+//     if (dayTimelineRef.current) {
+//       dayTimelineRef.current.expandAll(); // Trigger expand all before printing
+//     }
 //
-//     const printWindow = window.open('', '_blank', 'width=800,height=600');
-//     printWindow.document.open();
+//     setTimeout(() => {
+//       const printContent = document.querySelector('.day-timeline'); // Select the specific element you want to print
+//       if (!printContent) return; // If the element doesn't exist, exit the function
 //
-//     // Collect all stylesheets and inline styles
-//     const stylesheets = Array.from(document.querySelectorAll('link[rel="stylesheet"], style'))
-//         .map(stylesheet => stylesheet.outerHTML)
-//         .join('\n');
+//       const printWindow = window.open('', '_blank', 'width=800,height=600');
+//       printWindow.document.open();
 //
-//     // Add HTML structure and content
-//     printWindow.document.write(`
-//     <html>
-//       <head>
-//         <title>Print Day Timeline</title>
-//         ${stylesheets}
-//       </head>
-//       <body>
-//         ${printContent.outerHTML}
-//       </body>
-//     </html>
-//   `);
+//       // Collect all stylesheets and inline styles
+//       const stylesheets = Array.from(document.querySelectorAll('link[rel="stylesheet"], style'))
+//           .map(stylesheet => stylesheet.outerHTML)
+//           .join('\n');
 //
-//     printWindow.document.close(); // Close the document to finish loading
-//     printWindow.focus(); // Focus the new window
+//       // Add HTML structure and content
+//       printWindow.document.write(`
+//       <html>
+//         <head>
+//           <title>Print Day Timeline</title>
+//           ${stylesheets}
+//         </head>
+//         <body>
+//           ${printContent.outerHTML}
+//         </body>
+//       </html>
+//     `);
 //
-//     printWindow.onload = function () {
-//       printWindow.print(); // Trigger the print dialog
-//       printWindow.close(); // Close the print window after printing
-//     };
+//       printWindow.document.close(); // Close the document to finish loading
+//       printWindow.focus(); // Focus the new window
+//
+//       printWindow.onload = function () {
+//         printWindow.print(); // Trigger the print dialog
+//         printWindow.close(); // Close the print window after printing
+//       };
+//     }, 500); // Add a 500ms delay before printing
 //   };
 //
-//
-//
 //   return (
-//     <ScrollArea className="flex flex-col h-full w-full">
-//       <TooltipProvider>
-//         <div className="App flex h-screen overflow-hidden">
-//           <div className="flex-1 flex flex-col overflow-y-auto">
-//             <div className="flex-1 flex flex-col p-4 md:p-8 pt-6 h-full w-full">
-//               <div className="flex items-center justify-between mb-4">
-//                 <h2 className="text-3xl font-bold tracking-tight">
-//                   Plan Your Trip
-//                 </h2>
-//                 <Button onClick={handlePrint} className="mb-4">
-//                   Share Trip
-//                 </Button>
-//               </div>
-//               <Tabs
-//                 defaultValue="overview"
-//                 className="flex-1 flex flex-col h-full"
-//               >
-//                 <TabsList>
-//                   <TabsTrigger value="overview">Overview</TabsTrigger>
-//                   {dayCards.map((day, index) => (
-//                     <TabsTrigger key={day._id} value={`day-${index + 1}`}>
-//                       Day {index + 1}
-//                     </TabsTrigger>
-//                   ))}
-//                 </TabsList>
-//                 <TabsContent value="overview" className="flex-1 flex h-full">
-//                   <div className="w-full md:w-2/3 flex flex-col h-full pr-4">
-//                     <Card className="flex-1 flex flex-col h-full">
-//                       <CardHeader>
-//                         <CardTitle>Overview Map</CardTitle>
-//                       </CardHeader>
-//                       <CardContent className="flex-1">
-//                         <MapWithMarkers locations={allLocations} />
-//                       </CardContent>
-//                     </Card>
-//                   </div>
-//                   <div className="w-full md:w-1/3 h-full">
-//                     <div ref={printRef}>
-//                       <DayTimeline
-//                         className="h-full"
-//                         tripId={tripId}
-//                         onDaysUpdated={() =>
-//                           dispatch(fetchDayCards({ tripId, token }))
-//                         }
-//                       />
-//                     </div>
-//                   </div>
-//                 </TabsContent>
-//                 {dayCards.map((day, index) => (
-//                   <TabsContent
-//                     key={day._id}
-//                     value={`day-${index + 1}`}
-//                     className="flex-1 flex h-full"
-//                   >
+//       <ScrollArea className="flex flex-col h-full w-full">
+//         <TooltipProvider>
+//           <div className="App flex h-screen overflow-hidden">
+//             <div className="flex-1 flex flex-col overflow-y-auto">
+//               <div className="flex-1 flex flex-col p-4 md:p-8 pt-6 h-full w-full">
+//                 <div className="flex items-center justify-between mb-4">
+//                   <h2 className="text-3xl font-bold tracking-tight">
+//                     Plan Your Trip
+//                   </h2>
+//                   <Button onClick={handlePrint} className="mb-4">
+//                     Share Trip
+//                   </Button>
+//                 </div>
+//                 <Tabs
+//                     defaultValue="overview"
+//                     className="flex-1 flex flex-col h-full"
+//                 >
+//                   <TabsList>
+//                     <TabsTrigger value="overview">Overview</TabsTrigger>
+//                     {dayCards.map((day, index) => (
+//                         <TabsTrigger key={day._id} value={`day-${index + 1}`}>
+//                           Day {index + 1}
+//                         </TabsTrigger>
+//                     ))}
+//                   </TabsList>
+//                   <TabsContent value="overview" className="flex-1 flex h-full">
 //                     <div className="w-full md:w-2/3 flex flex-col h-full pr-4">
 //                       <Card className="flex-1 flex flex-col h-full">
 //                         <CardHeader>
-//                           <CardTitle>Day {index + 1} Map</CardTitle>
+//                           <CardTitle>Overview Map</CardTitle>
 //                         </CardHeader>
 //                         <CardContent className="flex-1">
-//                           <MapWithMarkers locations={day.locations} />
+//                           <MapWithMarkers locations={allLocations} />
 //                         </CardContent>
 //                       </Card>
 //                     </div>
 //                     <div className="w-full md:w-1/3 h-full">
-//                       <DayTimeline
-//                         className="h-full"
-//                         tripId={tripId}
-//                         dayId={day._id}
-//                         onDaysUpdated={() =>
-//                           dispatch(fetchDayCards({ tripId, token }))
-//                         }
-//                       />
+//                       <div ref={printRef}>
+//                         <DayTimeline
+//                             className="h-full"
+//                             tripId={tripId}
+//                             ref={dayTimelineRef} // Pass the ref to DayTimeline
+//                             onDaysUpdated={() =>
+//                                 dispatch(fetchDayCards({ tripId, token }))
+//                             }
+//                         />
+//                       </div>
 //                     </div>
 //                   </TabsContent>
-//                 ))}
-//               </Tabs>
+//                   {dayCards.map((day, index) => (
+//                       <TabsContent
+//                           key={day._id}
+//                           value={`day-${index + 1}`}
+//                           className="flex-1 flex h-full"
+//                       >
+//                         <div className="w-full md:w-2/3 flex flex-col h-full pr-4">
+//                           <Card className="flex-1 flex flex-col h-full">
+//                             <CardHeader>
+//                               <CardTitle>Day {index + 1} Map</CardTitle>
+//                             </CardHeader>
+//                             <CardContent className="flex-1">
+//                               <MapWithMarkers locations={day.locations} />
+//                             </CardContent>
+//                           </Card>
+//                         </div>
+//                         <div className="w-full md:w-1/3 h-full">
+//                           <DayTimeline
+//                               className="h-full"
+//                               tripId={tripId}
+//                               dayId={day._id}
+//                               onDaysUpdated={() =>
+//                                   dispatch(fetchDayCards({ tripId, token }))
+//                               }
+//                           />
+//                         </div>
+//                       </TabsContent>
+//                   ))}
+//                 </Tabs>
+//               </div>
 //             </div>
 //           </div>
-//         </div>
-//       </TooltipProvider>
-//     </ScrollArea>
+//         </TooltipProvider>
+//       </ScrollArea>
 //   );
 // };
 //
@@ -204,7 +197,7 @@ import {
 } from "../components/ui/card";
 import MapWithMarkers from "../services/api/map-template";
 import DayTimeline from "../components/dayTimeline";
-import { fetchDayCards, clearDayCards } from "../redux/dayTimelineSlice";
+import { fetchDayCards } from "../redux/dayTimelineSlice";
 import { Button } from "../components/ui/button";
 
 const AddTrip = () => {
@@ -213,14 +206,11 @@ const AddTrip = () => {
   const token = useSelector((state) => state.auth.token);
   const dayCards = useSelector((state) => state.dayTimeline.dayCards);
   const [allLocations, setAllLocations] = useState([]);
+  const [activeTab, setActiveTab] = useState("overview"); // Track the active tab
   const printRef = useRef(); // Reference for the day cards section
-  const dayTimelineRef = useRef(); // Ref to DayTimeline
 
   useEffect(() => {
     if (tripId && token) {
-      // Clear dayCards and allLocations when tripId changes
-      setAllLocations([]);
-      dispatch(clearDayCards()); // Clear the existing day cards
       dispatch(fetchDayCards({ tripId, token }));
     }
   }, [dispatch, tripId, token]);
@@ -231,87 +221,19 @@ const AddTrip = () => {
         return [...acc, ...day.locations];
       }, []);
       setAllLocations(locations);
-    } else {
-      setAllLocations([]); // Ensure locations are cleared if there are no dayCards
     }
   }, [dayCards]);
 
-  // const handlePrint = () => {
-  //   if (dayTimelineRef.current) {
-  //     dayTimelineRef.current.expandAll(); // Trigger expand all before printing
-  //   }
-  //
-  //   const printContent = document.querySelector('.day-timeline'); // Select the specific element you want to print
-  //   if (!printContent) return; // If the element doesn't exist, exit the function
-  //
-  //   const printWindow = window.open('', '_blank', 'width=800,height=600');
-  //   printWindow.document.open();
-  //
-  //   // Collect all stylesheets and inline styles
-  //   const stylesheets = Array.from(document.querySelectorAll('link[rel="stylesheet"], style'))
-  //       .map(stylesheet => stylesheet.outerHTML)
-  //       .join('\n');
-  //
-  //   // Add HTML structure and content
-  //   printWindow.document.write(`
-  //   <html>
-  //     <head>
-  //       <title>Print Day Timeline</title>
-  //       ${stylesheets}
-  //     </head>
-  //     <body>
-  //       ${printContent.outerHTML}
-  //     </body>
-  //   </html>
-  // `);
-  //
-  //   printWindow.document.close(); // Close the document to finish loading
-  //   printWindow.focus(); // Focus the new window
-  //
-  //   printWindow.onload = function () {
-  //     printWindow.print(); // Trigger the print dialog
-  //     printWindow.close(); // Close the print window after printing
-  //   };
-  // };
-
   const handlePrint = () => {
-    if (dayTimelineRef.current) {
-      dayTimelineRef.current.expandAll(); // Trigger expand all before printing
-    }
-
     setTimeout(() => {
-      const printContent = document.querySelector('.day-timeline'); // Select the specific element you want to print
-      if (!printContent) return; // If the element doesn't exist, exit the function
+      const printContent = printRef.current.innerHTML;
+      const originalContent = document.body.innerHTML;
 
-      const printWindow = window.open('', '_blank', 'width=800,height=600');
-      printWindow.document.open();
-
-      // Collect all stylesheets and inline styles
-      const stylesheets = Array.from(document.querySelectorAll('link[rel="stylesheet"], style'))
-          .map(stylesheet => stylesheet.outerHTML)
-          .join('\n');
-
-      // Add HTML structure and content
-      printWindow.document.write(`
-      <html>
-        <head>
-          <title>Print Day Timeline</title>
-          ${stylesheets}
-        </head>
-        <body>
-          ${printContent.outerHTML}
-        </body>
-      </html>
-    `);
-
-      printWindow.document.close(); // Close the document to finish loading
-      printWindow.focus(); // Focus the new window
-
-      printWindow.onload = function () {
-        printWindow.print(); // Trigger the print dialog
-        printWindow.close(); // Close the print window after printing
-      };
-    }, 500); // Add a 500ms delay before printing
+      document.body.innerHTML = printContent;
+      window.print();
+      document.body.innerHTML = originalContent;
+      window.location.reload(); // Reload the page to restore the original content
+    }, 500);
   };
 
   return (
@@ -324,13 +246,23 @@ const AddTrip = () => {
                   <h2 className="text-3xl font-bold tracking-tight">
                     Plan Your Trip
                   </h2>
-                  <Button onClick={handlePrint} className="mb-4">
+                  <Button
+                      onClick={handlePrint}
+                      className="mb-4"
+                      disabled={activeTab !== "overview"} // Disable if not on overview
+                      style={{
+                        backgroundColor:
+                            activeTab !== "overview" ? "grey" : "", // Grey out the button
+                        cursor: activeTab !== "overview" ? "not-allowed" : "pointer",
+                      }}
+                  >
                     Share Trip
                   </Button>
                 </div>
                 <Tabs
                     defaultValue="overview"
                     className="flex-1 flex flex-col h-full"
+                    onValueChange={(value) => setActiveTab(value)} // Track active tab
                 >
                   <TabsList>
                     <TabsTrigger value="overview">Overview</TabsTrigger>
@@ -356,7 +288,6 @@ const AddTrip = () => {
                         <DayTimeline
                             className="h-full"
                             tripId={tripId}
-                            ref={dayTimelineRef} // Pass the ref to DayTimeline
                             onDaysUpdated={() =>
                                 dispatch(fetchDayCards({ tripId, token }))
                             }
@@ -402,3 +333,4 @@ const AddTrip = () => {
 };
 
 export default AddTrip;
+
